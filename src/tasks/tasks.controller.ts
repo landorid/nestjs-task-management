98bd8fb3cc1603a5aa1bc5b17ff3c26d73b5c3c1
Lándori Dávid <info@honlapvarazslo.com>
2022-11-23 19:37:40 +0100

    ✨   Add task controller validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-statud.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -37,15 +38,18 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    return this.tasksService.deleteTask(id);
+  deleteTask(@Param('id') id: string): Task {
+    const result = this.tasksService.getTaskById(id);
+    this.tasksService.deleteTask(id);
+
+    return result;
   }
 
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() params: UpdateTaskStatusDto,
   ): Task {
-    return this.tasksService.updateTaskStatus(id, status);
+    return this.tasksService.updateTaskStatus(id, params.status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -35,7 +35,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const result = this.tasks.find((task) => task.id === id);
+
+    if (!result) {
+      throw new NotFoundException(`Task with ${id} not found`);
+    }
+
+    return result;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {

116edc017bf684b705923612777ea16ac6a69205
Lándori Dávid <info@honlapvarazslo.com>
2022-11-25 22:59:32 +0100

    ✨   Add jwt token auth to task controller

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jtw.strategy';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      //TODO: move to env variable
+      secret: 'very-safe-secret-1234',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [JwtModule, PassportModule],
+})
+export class AuthModule {}
